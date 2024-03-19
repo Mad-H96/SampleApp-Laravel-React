a1c2645631a30803ea9835560cc2a610b26b1ad4
Mad-H96 <madawa96sl123@gmail.com>
2024-03-19 22:04:35 +0530

    customerController

---
+++ app/Models/Customer.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+    
+        'email',
+        'password',
+        'first_name',
+        'last_name',
+        'phone_number',
+        'zip_code',
+       
+    ];
+}
